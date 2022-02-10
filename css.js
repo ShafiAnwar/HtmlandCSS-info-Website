96d5b9b9761a58550af4e051d2f3ec6483ab0a4a
ShafiAnwar <92093225+ShafiAnwar@users.noreply.github.com>
2022-02-10 18:53:31 +0530

    Add files via upload

---
+++ css.js
@@ -0,0 +1,13 @@
+function next(){
+   
+ window.location="https://en.wikipedia.org/wiki/Dennis_Ritchie";
+}
+
+function next2(){
+    window.location="https://en.wikipedia.org/wiki/Ada_Lovelace";
+}
+
+function ne(){
+    window.location="https://en.wikipedia.org/wiki/Gennady_Korotkevich";
+}
+
